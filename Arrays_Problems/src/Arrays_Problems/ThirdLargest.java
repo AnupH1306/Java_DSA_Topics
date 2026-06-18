853a53d3baf4eb1a15fdd5e3f8f90b27ee8333e7
AnupH1306 <anuphanabar1306@gmail.com>
2026-06-18 17:12:15 +0530

    Edited Succescfully

--- Arrays_Problems/src/Arrays_Problems/ThirdLargest.java
+++ Arrays_Problems/src/Arrays_Problems/ThirdLargest.java
@@ -1,5 +1,43 @@
 package Arrays_Problems;
 
 public class ThirdLargest {
+	
+	public static void main(String[] args) {
+		
+		//Declare An Array(Must be Greater than zero;
+		int[] arr = {};
+		
+		int first = -1;
+		int second =-1; 
+		int third = -1;
+		
+		for(int i=0; i<arr.length;i++) {
+			
+			
+			//Skips duplicate elements
+			if(arr[i] == first || arr[i] == second || arr[i] == third ) {
+				continue;
+			}
+			
+			if(arr[i] > first) {
+				third = second;
+				second = first;
+				first = arr[i];
+			}
+			
+			else if(arr[i]> second) {
+				third = second;
+				second = arr[i];
+			}else if(arr[i] >third){
+				third = arr[i];
+			}
+		}
+		
+		if(third == -1) {
+			System.out.println("No element");
+		}else {
+			System.out.println("Third Largest Elemet is: " + third);
+		}
+	}
 
 }
