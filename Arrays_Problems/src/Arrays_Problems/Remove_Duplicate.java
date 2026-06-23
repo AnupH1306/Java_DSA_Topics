45f465f25789054875ddea07dd355d8af2e9d3ba
AnupH1306 <anuphanabar1306@gmail.com>
2026-06-23 08:42:16 +0530

    Solved Remove Duplicate Array Quetion on LC-26

---
+++ Arrays_Problems/src/Arrays_Problems/Remove_Duplicate.java
@@ -0,0 +1,26 @@
+package Arrays_Problems;
+
+public class Remove_Duplicate {
+	
+	public static void main(String[] args) {
+		
+		//take an array
+		int []arr = {1,1,2,2,3};
+		
+		//Take variable j & i And initialize it to 0 & 1.
+		int j=0;
+		int i=1;
+		
+		for(;i<arr.length;i++) {
+			if(arr[j] != arr[i]) {
+				arr[++j] = arr[i];
+			}
+		}
+		
+		//Print final array upto the j that we are removed duplicates.
+		for(int k=0;k<=j;k++) {
+			System.out.println(arr[k]);
+		}
+	}
+
+}
