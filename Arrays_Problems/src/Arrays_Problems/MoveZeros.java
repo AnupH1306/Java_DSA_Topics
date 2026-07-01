b8b668eab9c32540344586323cc02f54bc625b3e
AnupH1306 <anuphanabar1306@gmail.com>
2026-07-01 23:47:27 +0530

    2 Problem has solved TOday

--- Arrays_Problems/src/Arrays_Problems/MoveZeros.java
+++ Arrays_Problems/src/Arrays_Problems/MoveZeros.java
@@ -3,26 +3,29 @@ package Arrays_Problems;
 public class MoveZeros {
 	
 	public static void main(String [] args) {
-		
-		int [] arr = {0,0,1,2,3,4,5,6,6};
-		
+		int[] arr = {1,0,2,0,3};
 		int i=1;
 		int j=0;
 		int temp =0;
-		
-		while(i<arr.length && j<i) {
-			
-			if(arr[i] !=0 && arr[j]==0) {
-				temp = arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-				j++;;
-			}else {
-				i++;
-			}
+		while (i < arr.length && j < i) {
+
+		    if (arr[i] != 0 && arr[j] == 0) {
+		    	  temp = arr[i];
+		         arr[i] = arr[j];
+		         arr[j] = temp;
+		        i++;
+		        j++;
+		    }
+		    else if (arr[j] == 0) {
+		        i++;
+		    }
+		    else {
+		        i++;
+		        j++;
+		    }
 		}
 	
-	for(i=0; i<arr.length;i++) {
+	for( i=0; i<arr.length;i++) {
 		System.out.println(arr[i]);
 	}
 	}
