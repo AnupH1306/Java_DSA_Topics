7e08d246f461293887812d214bae32ffceb736ff
AnupH1306 <anuphanabar1306@gmail.com>
2026-07-28 08:08:26 +0530

    Added optimal Solution for single element problem using XOR method

--- FindSingleElement.java
+++ FindSingleElement.java
@@ -6,29 +6,36 @@ public class FindSingleElement {
 		//Brute Force Solution
 		int []arr = {1,1,2,2,3,4,4,5,5};
 		
-		int cnt = 0;
-		int num = 0;
+//		int cnt = 0;
+//		int num = 0;
+//		for(int i=0; i<arr.length;i++) {
+//			 num = arr[i];
+//			 
+//			 for(int j =0;j<arr.length;j++) {
+//				 if(arr[j] == num) {
+//					 cnt++;
+//				 }
+//			 }
+//			 if(cnt == 1) {
+//				 System.out.println(arr[i]);
+//				 break;
+//			 }
+//			 cnt = 0;
+//		}
+//		if(cnt == 0) {
+//			System.out.println("No element found with one appearence");
+//		}
+//		
+	//Optimal Solution	
+		int xor = 0;
+		
 		for(int i=0; i<arr.length;i++) {
-			 num = arr[i];
-			 
-			 for(int j =0;j<arr.length;j++) {
-				 if(arr[j] == num) {
-					 cnt++;
-				 }
-			 }
-			 if(cnt == 1) {
-				 System.out.println(arr[i]);
-				 break;
-			 }
-			 cnt = 0;
+			xor = xor ^ arr[i];
+			
 		}
-		
-		
-		
-		if(cnt == 0) {
-			System.out.println("No element found with one appearence");
-		}
-		
+		System.out.println(xor);
 	}
+	
+	
 
 }
