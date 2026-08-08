bb1bf46fc95b11c21f1839e569c741014a117d72
AnupH1306 <anuphanabar1306@gmail.com>
2026-08-08 13:11:35 +0530

    Maximu Subarray Sum  Problem Solved by using the Kadane's Algorithm

---
+++ MaximumSubArraySum.java
@@ -0,0 +1,30 @@
+package Arrays_Problems;
+
+public class MaximumSubArraySum {
+	
+	public static void main(String[] args) {
+		
+		//Approaches to solve this problem 1. Brute force 1. Better 3. Optimal
+		
+		//Optimal Solution (kadene's Algorithm)
+		int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+		int max = Integer.MIN_VALUE;
+		
+		int sum = 0;
+		
+		for(int i=0; i<arr.length; i++) {
+			sum += arr[i];
+			
+			if(sum > max) {
+				max = Math.max(sum, max);
+			}
+			
+			if(sum < 0) {
+				sum = 0;
+			}
+		}
+		
+		System.out.println("Maximun Sub-Array Sum in the given array is:"+" "+ max);
+	}
+
+}
