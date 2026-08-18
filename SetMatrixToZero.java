1cc16cc8f58a93e4228b4e8dcc370f9ae5500772
AnupH1306 <anuphanabar1306@gmail.com>
2026-08-18 23:38:04 +0530

    Bruet force Solution for set matrix to Zero

---
+++ SetMatrixToZero.java
@@ -0,0 +1,65 @@
+package Arrays_Problems;
+
+import java.util.Scanner;
+
+public class SetMatrixToZero {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = 4;
+		int m = 4;
+		int[][] matrix = new int[n][m];
+		
+		System.out.println("Fill the array with 0 And 1");
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		
+		// Step 1: Track Matrix And find Zeros
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				if(matrix[i][j] == 0) {
+					markRow(i, m, matrix);
+					markCol(j, n, matrix);
+				}
+			}
+		}
+		
+		// Step 2: Convert all temporary -1 markers to 0 (THIS WAS MISSING)
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				if(matrix[i][j] == -1) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+
+		// Step 3: Print the matrix
+		for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        sc.close();
+	}
+
+	public static void markRow(int i, int m, int[][] matrix) {
+		for(int j=0;j<m;j++) {
+			if(matrix[i][j] != 0) {
+				matrix[i][j] = -1;
+			}
+		}
+	}
+
+	public static void markCol(int j, int n, int[][] matrix) {
+		for(int i=0;i<n;i++) {
+			if(matrix[i][j] != 0) {
+				matrix[i][j] = -1;
+			}
+		}
+	}
+}
